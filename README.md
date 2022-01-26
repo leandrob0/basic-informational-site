1ca48354cf13e134bd15b1b401d30f9a5b06b5a2
leandrob0 <leandro.bovino@gmail.com>
2022-01-25 21:37:44 -0300

    finished

---
+++ README.md
@@ -0,0 +1,1 @@
+Very basic node project to start learning.
--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = 'localhost';
 const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello, World!</h1>');
+    if(req.url === '/') {
+        fs.readFile('./visuals/index.html', (err, data) => {
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        })
+    } else if(req.url === '/about') {
+        fs.readFile('./visuals/about.html', (err, data) => {
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        })
+    } else if(req.url === '/contact-me') {
+        fs.readFile('./visuals/contact-me.html', (err, data) => {
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        })
+    } else {
+        fs.readFile('./visuals/404.html', (err, data) => {
+            res.writeHead(404,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        })
+    }
 })
 
 server.listen(port, hostname, () => {
