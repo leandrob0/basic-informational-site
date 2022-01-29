cb3d6b0fdf502716f9b7c4a9cdb7f765c6588d51
leandrob0 <leandro.bovino@gmail.com>
2022-01-28 23:57:15 -0300

    changed to an express app

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
 
 const hostname = 'localhost';
 const port = process.env.PORT || 8080;
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        fs.readFile('./visuals/index.html', (err, data) => {
-            if(err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end;
-        })
-    } else if(req.url === '/about') {
-        fs.readFile('./visuals/about.html', (err, data) => {
-            if(err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end;
-        })
-    } else if(req.url === '/contact-me') {
-        fs.readFile('./visuals/contact-me.html', (err, data) => {
-            if(err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end;
-        })
-    } else {
-        fs.readFile('./visuals/404.html', (err, data) => {
-            res.writeHead(404,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end;
-        })
-    }
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/visuals/index.html'));
 })
 
-server.listen(port, hostname, () => {
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname+'/visuals/about.html'));
+})
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile(path.join(__dirname+'/visuals/contact-me.html'));
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/visuals/404.html'));
+})
+
+app.listen(port, hostname, () => {
     console.log(`Server running at ${hostname}:${port}/`)
 })
